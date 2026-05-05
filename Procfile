14330fa35acc2bc5a8d4d930528c49dfccfaa7f1
gfj2e <gfj2e@outlook.com>
2026-05-04 21:37:42 -0500

    Some deployment changes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn stmc.wsgi --bind 0.0.0.0:$PORT --log-file -
-release: python manage.py migrate --noinput
+release: python manage.py migrate --noinput && python manage.py collectstatic --noinput
--- stmc/settings.py
+++ stmc/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django_htmx.middleware.HtmxMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -198,3 +199,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+}
